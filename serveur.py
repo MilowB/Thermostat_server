48ae19e32bb05432f079adb2713efe0673520c0b
Mickael <mickael.bettinelli@protonmail.com>
2018-12-03 21:32:58 +0100

    gestion d'erreurs et ajout de routes

--- serveur.py
+++ serveur.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask import abort
 from flask_httpauth import HTTPBasicAuth
+from flask import request
 auth = HTTPBasicAuth()
 
 app = Flask(__name__)
@@ -15,11 +16,53 @@ def getTemperature(key, phrase):
         abort(401)
     return jsonify({'id': phrase})
 
+@app.route("/data/planning/<string:key>", methods=['GET'])
+def getPlanning(key):
+    if not auth(key):
+        abort(401)
+    return jsonify({'id': 0})
+
 @app.route("/data/history/<string:key>", methods=['GET'])
 def getHistory(key):
     if not auth(key):
         abort(401)
     return jsonify({'id': 0})
+
+'''
+key : authentification key to access the API
+PUT - rules : json rules
+'''
+@app.route("/manage/planning/<string:key>", methods=['PUT'])
+def setTemperatureRules(key):
+    if not auth(key):
+        abort(401)
+    elif not "rules" in request.json:
+        abort(400)
+    return jsonify(request.json)
+
+'''
+key : authentification key to access the API
+PUT - temperature : temperature to set
+'''
+@app.route("/manage/currentTemperature/<string:key>", methods=['PUT'])
+def setCurrentTemperature(key):
+    if not auth(key):
+        abort(401)
+    elif not "temperature" in request.json:
+        abort(400)
+    return jsonify(request.json)
+
+'''
+key : authentification key to access the API
+PUT - working : activate or desactivate the thermostat
+'''
+@app.route("/manage/thermostat/<string:key>", methods=['PUT'])
+def setWorking(key):
+    if not auth(key):
+        abort(401)
+    elif not "working" in request.json:
+        abort(400)
+    return jsonify(request.json)
 
 def auth(key):
     file = open("config.txt", "r") 
